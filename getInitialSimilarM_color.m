811127132f283cf3ad9a3cccec1ca67badefb263
YiqingZhang <ichingchang@foxmail.com>
2014-12-06 11:19:42 +0800

    give user choice to use cosine distance

--- getInitialSimilarM_color.m
+++ getInitialSimilarM_color.m
@@ -3,6 +3,8 @@ function [ similarM_color ] = getInitialSimilarM_color( neighbours,histM )
 %   neighbours: the neighbour matrix got by segmentation
 %   histM: the histogram matrix
 
+cosineDistanceFlag = 1; %this is a flag indicating we are using the cosine distance
+
 pos = find(neighbours);
 similarM_color = zeros(size(neighbours));
 
@@ -11,7 +13,12 @@ for i = 1: length(pos)
     [row,col] = ind2sub(size(neighbours),index);
     if col > row % the col should always be larger than the row
         %fprintf('%d %d %d %d\n',index,row,col,neighbours(row,col)); 
-        similarM_color(index) = sum( min(histM(row,:),histM(col,:)) );
+        if cosineDistanceFlag > 0
+            tmpHist = [histM(row,:);histM(col,:)];
+            similarM_color(index) = 1 - pdist(tmpHist,'cosine');
+        else
+            similarM_color(index) = sum( min(histM(row,:),histM(col,:)) );
+        end
     end
 end
 
--- updateSimilarityM_color.m
+++ updateSimilarityM_color.m
@@ -1,3 +1,4 @@
+
 function [ similarM_color] = updateSimilarityM_color(similarM_color,histM,firstBlob)
 %updateSimilarityM_color updates the similarity matrix M of color
 %   this function updates the similarity matrix of the color
@@ -6,13 +7,21 @@ function [ similarM_color] = updateSimilarityM_color(similarM_color,histM,firstB
 %   histM: the histogram matrix !!! you must provide the
 %   new sizeM vector to this function !!!
 
+cosineDistanceFlag = 1; %this is a flag indicating we are using the cosine distance
+
     %we need to recalculate the similatity matrix
 
     for i = firstBlob + 1 : size(similarM_color,2)%this deal with the row(horizontal line)
         rowTmp = firstBlob;
         colTmp = i;
         if(similarM_color(rowTmp,colTmp) > 0) %there is the edge
-            similarM_color(rowTmp,colTmp) =  sum( min(histM(rowTmp,:),histM(colTmp,:)) );
+            
+            if cosineDistanceFlag > 0
+                tmpHist = [histM(rowTmp,:);histM(colTmp,:)];
+                similarM_color(rowTmp,colTmp) = 1 - pdist(tmpHist,'cosine');
+            else
+                similarM_color(rowTmp,colTmp) =  sum( min(histM(rowTmp,:),histM(colTmp,:)) );
+            end
         end
     end %this pair is the standard pair of accessing one's neighbours
 
@@ -20,7 +29,13 @@ function [ similarM_color] = updateSimilarityM_color(similarM_color,histM,firstB
         rowTmp = i;
         colTmp = firstBlob;
         if(similarM_color(rowTmp,colTmp) > 0) %recauculate the number
-            similarM_color(rowTmp,colTmp) = sum( min(histM(rowTmp,:),histM(colTmp,:)) );
+            
+            if cosineDistanceFlag > 0
+                tmpHist = [histM(rowTmp,:);histM(colTmp,:)];
+                similarM_color(rowTmp,colTmp) = 1 - pdist(tmpHist,'cosine');
+            else
+                similarM_color(rowTmp,colTmp) = sum( min(histM(rowTmp,:),histM(colTmp,:)) );
+            end
         end
 
     end
